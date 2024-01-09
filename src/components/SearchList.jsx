47a317481b473103c23199c16bbf4c941e5a5dd1
Ivansancor <ivanvaldeolivo95@gmail.com>
2024-01-09 01:52:49 +0100

    added some styling to searchlist div

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
 
-export default function SearchList( {matchedPokemon}) {
+export default function SearchList( {matchedPokemon, targetValue}) {
     const matchedPokes = matchedPokemon.map(poke => <li key={poke}><Link to={`/pokemon/poke/${poke}`}>{poke}</Link></li>);
 
     return (
         <ul className="matched-list">
-            {matchedPokemon.length === 0 ? <span>No such Pokemon found...</span> : matchedPokes}
+            {matchedPokemon.length === 0 && targetValue !== "" ? <span>No such Pokemon found...</span> : matchedPokes}
         </ul>
     )
 }
--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Link, Outlet, useParams, useLoaderData, useSearchParams } from "react-router-dom"
 import { getAllPokemon } from '../utils/api'
 import SearchList from '../components/SearchList'
@@ -12,21 +12,26 @@ export default function Pokemon() {
     const SearchListPRef = useRef()
     const params = useParams();
     const [searchParams, setSearchParams] = useSearchParams();
-    const [matchedPokemon, SetMatchedPokemon] = useState(searchParams.getAll("search") || [])
+    const [matchedPokemon, setMatchedPokemon] = useState(searchParams.getAll("search") || [])
     const [targetValue, setTargetValue] = useState("")
+    
+    
 
     function handleSearch(e) {
-        if (e.target.value === "") {
-            SetMatchedPokemon([]);
+            if (e.target.value === "") {
+            setMatchedPokemon([]);
+            setTargetValue("");
+            setSearchParams({});
             SearchListPRef.current.style.display = "none";
         } else {
             SearchListPRef.current.style.display = "flex";
             const matchedPokemon = allPokemon.results.map(poke => poke.name).filter(poke => poke.includes(e.target.value.toLowerCase()));
-            SetMatchedPokemon(matchedPokemon);
+            setMatchedPokemon(matchedPokemon);
             setSearchParams( {search: matchedPokemon} );
-            setTargetValue(e.target.value)
+            setTargetValue(e.target.value);
         }
     }
+
 
     return (
         params.page || params. id ? <Outlet /> :
@@ -35,12 +40,12 @@ export default function Pokemon() {
                 <div className="pokemon-div">
                     <p><label htmlFor="searchbar">Perform a search: </label>
                         <input id="searchbar" 
-                            type="text" 
-                            value = {targetValue}
+                            type="text"
                             placeholder="Type name of Pokemon or ID"
+                            value = {targetValue}
                             onChange={handleSearch}/>
                     </p>
-                    <div ref={SearchListPRef} style={matchedPokemon.length > 0 ? {display:"flex"} : {display: "none"}}><SearchList matchedPokemon={matchedPokemon} /></div>
+                    <div ref={SearchListPRef} style={matchedPokemon.length > 0 ? {display:"flex"} : {display: "none"}}><SearchList matchedPokemon={matchedPokemon} targetValue={targetValue} /></div>
                     <p>or</p>
                     <p><Link to="1"><button className="pagination-btn">Go to Pokemon List</button></Link></p>
                 </div>
